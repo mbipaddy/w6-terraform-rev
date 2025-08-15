85081adc1da6a165a0d25ef30410e07a4d9f2278
MBI PADDY <mbiaddy@gmail.com>
2025-08-15 02:34:33 +0200

    Chnage

--- terraform-remote-backend/backend.tf
+++ terraform-remote-backend/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "week6-mpek.bucket"
+    bucket         = "bucket-name"
     key            = "dev/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-lock"
+    dynamodb_table = "table-name"
     encrypt        = true
   }
 }
